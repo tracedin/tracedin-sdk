65b4f0f027ceba183fc822f1369d7543c01bcb99
강지원 <kangji149@naver.com>
2024-10-28 15:42:48 +0900

    refator : 5% 확률로 이상치 생성

--- example/tracedin-client/src/main/java/com/univ/tracedinclient/SleepAspect.java
+++ example/tracedin-client/src/main/java/com/univ/tracedinclient/SleepAspect.java
@@ -14,7 +14,10 @@ public class SleepAspect {
 
     @Before("execution(* com.univ.tracedinclient..*(..))")
     public void addSleep() throws InterruptedException {
-        int sleepTime = 100 + random.nextInt(400);
-        Thread.sleep(sleepTime);
+        if (random.nextInt(100) < 5) { // 0~4일 때, 5% 확률
+            int sleepTime = 1000 + random.nextInt(1000); // 1000ms~2000ms (1~2초)
+            Thread.sleep(sleepTime);
+        }
+        // 나머지 95% 확률의 경우는 대기 없음
     }
 }
